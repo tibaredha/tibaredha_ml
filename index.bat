69f39b9f334973c2c1af313d2cb04f095835934a
tibaredha <tibaredha@yahoo.fr>
2021-03-13 11:26:18 +0100

    maj

---
+++ index.bat
@@ -0,0 +1,28 @@
+::tiba batch
+:: tiba batch
+::@echo off
+title TIBA BATCH
+mode con cols=80 lines=10
+color 1F
+
+
+
+start http://127.0.0.1:5000/
+
+python main.py
+
+
+::pause > nul
+
+exit
+
+
+
+
+
+
+
+
+
+
+
